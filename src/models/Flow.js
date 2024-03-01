1798ceb4630cdd0184fdc55045940259297bdcaa
csfx-py <csfxshreyas@gmail.com>
2024-03-02 00:42:48 +0530

    added owner to flow model

--- src/models/Flow.js
+++ src/models/Flow.js
@@ -1,6 +1,11 @@
 const mongoose = require("mongoose");
 
 const flowSchema = new mongoose.Schema({
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
   name: {
     type: String,
     required: true,
